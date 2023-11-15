36a896378166f986313ca191b31b52d3cb9f3118
Samnath <116250941+Samnathsha@users.noreply.github.com>
2023-11-15 13:59:42 +0530

    Add files via upload

---
+++ mainx.java
@@ -0,0 +1,11 @@
+ import java.util.Scanner;
+
+public class mainx {
+    public static void main(String[] args) {
+        int a =1;
+        int b =2;
+        int c = a+b
+System.out.println("ans " + c);
+
+        }
+        }
